a20ac89a42d42d338497ffad29ee57a5cd2811e3
Game Monster <gameofthemonster@gmail.com>
2026-04-07 23:57:47 +0800

    Revert rendering.rs, pob.rs

--- src/pob.rs
+++ src/pob.rs
@@ -138,6 +138,7 @@ impl PoBMode {
                 }
             }
             AppEvent::CharacterInput { ch } => {
+                let ch = if ch.is_ascii() { ch } else { '?' };
                 self.lua_instance
                     .handle_event(PoBEvent::Char(ch), &mut ctx)?;
             }
